b03837524744ec0c636547f54aa3cd8edd1e1f11
dandy <febrianod@gmail.com>
2019-11-07 14:29:30 +0700

    add_delete_controller

--- ruangan/src/main/java/apap/ta/ruangan/Controller/PengadaanFasilitasController.java
+++ ruangan/src/main/java/apap/ta/ruangan/Controller/PengadaanFasilitasController.java
@@ -1,11 +1,11 @@
 package apap.ta.ruangan.Controller;
 
-
 import apap.ta.ruangan.Model.PengadaanFasilitasModel;
 import apap.ta.ruangan.Service.PengadaanFasilitasService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,5 +25,20 @@ public class PengadaanFasilitasController {
         List<PengadaanFasilitasModel> listPengadaanFasilitas = pengadaanFasilitasService.getAllPengadaanFasilitas();
         model.addAttribute("listPengadaanFasilitas", listPengadaanFasilitas);
         return "view-all-pengadaan-fasilitas";
+    }
+
+    @RequestMapping(value = "/delete/{idPengadaanFasilitas}")
+    public String delete(
+        @PathVariable(value = "idPengadaanFasilitas") Long id, Model model){
+        PengadaanFasilitasModel pengadaanFasilitas = pengadaanFasilitasService.getPengadaanFasilitasById(id);
+        if(pengadaanFasilitas == null){
+            return "404";
+        }
+        else{
+        String nama = pengadaanFasilitas.getNama();
+        pengadaanFasilitasService.deletePengadaanFasilitas(id);
+        model.addAttribute("nama", nama);
+        return "delete-pengadaan-fasilitas";
+        }
     }
 }
