2f76afd5b4648b94b8edf1968a8b60aab3fd9969
dandy <febrianod@gmail.com>
2019-11-07 14:03:28 +0700

    edit_service_impl

--- ruangan/src/main/java/apap/ta/ruangan/Service/PengadaanFasilitasServiceImpl.java
+++ ruangan/src/main/java/apap/ta/ruangan/Service/PengadaanFasilitasServiceImpl.java
@@ -2,11 +2,16 @@ package apap.ta.ruangan.Service;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import apap.ta.ruangan.Model.PengadaanFasilitasModel;
 import apap.ta.ruangan.Repository.PengadaanFasilitasDb;
 
+@Service
+@Transactional
 public class PengadaanFasilitasServiceImpl implements PengadaanFasilitasService {
     @Autowired
     private PengadaanFasilitasDb pengadaanFasilitasDb;
