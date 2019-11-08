080ceed7e8ed95c236f96a25a4ddf4a1ead6fad1
feliarf <feliarisky23@gmail.com>
2019-11-08 16:25:33 +0700

    feat: add halaman utama

--- ruangan/src/main/java/apap/ta/ruangan/security/WebSecurityConfig.java
+++ ruangan/src/main/java/apap/ta/ruangan/security/WebSecurityConfig.java
@@ -21,6 +21,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
                 .authorizeRequests()
                 .antMatchers("/css/**").permitAll()
                 .antMatchers("/js/**").permitAll()
+                .antMatchers("/img/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
