134192a3048bb96c668fde5b8fd39d648b8eafca
dandy <febrianod@gmail.com>
2019-12-05 15:15:07 +0700

    edit

--- ruangan/src/main/java/apap/ta/ruangan/Controller/PengadaanFasilitasController.java
+++ ruangan/src/main/java/apap/ta/ruangan/Controller/PengadaanFasilitasController.java
@@ -61,7 +61,7 @@ public class PengadaanFasilitasController {
             listPengadaanFasilitas = user.getPengadaanFasilitasList();        
         }
         model.addAttribute("listPengadaanFasilitas", listPengadaanFasilitas);
-        model.addAttribute("deleted", deleted);
+        model.addAttribute("status", deleted);
         return "view-all-pengadaan-fasilitas";
         }
     }
@@ -94,7 +94,7 @@ public class PengadaanFasilitasController {
             listPengadaanFasilitas = user.getPengadaanFasilitasList();        
         }
         String berhasil = "Pengadaan fasilitas berhasil ditambahkan";
-        model.addAttribute("berhasil", berhasil);
+        model.addAttribute("status", berhasil);
         model.addAttribute("listPengadaanFasilitas", listPengadaanFasilitas);
         return "view-all-pengadaan-fasilitas";
     }
