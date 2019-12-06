317e8ca9edca216f0cbb22419334d5c0c97a911a
Aditya Nanda Tri Prakoso <adityanandaaa@gmail.com>
2019-12-06 12:15:24 +0700

    rombak besar besaran

--- src/main/java/apap/ta/ruangan/Service/PeminjamanRuanganService.java
+++ src/main/java/apap/ta/ruangan/Service/PeminjamanRuanganService.java
@@ -2,12 +2,14 @@ package apap.ta.ruangan.Service;
 
 import apap.ta.ruangan.Model.PeminjamanRuanganModel;
 import apap.ta.ruangan.Model.UserModel;
+import apap.ta.ruangan.Rest.PengajuanSuratModel;
 
+import java.text.ParseException;
 import java.util.List;
 
 public interface PeminjamanRuanganService {
 
-    void addPeminjamRuangan(PeminjamanRuanganModel peminjamanRuanganModel);
+    void addPeminjamRuangan(PeminjamanRuanganModel peminjamanRuanganModel, PengajuanSuratModel pengajuanSuratModel) throws ParseException;
     PeminjamanRuanganModel getPeminjamanRuanganById(Long idPeminjamanRuangan);
     List<PeminjamanRuanganModel> getPeminjamanRuanganList();
     PeminjamanRuanganModel ubahPersetujuan(Long idPeminjamanRuangan, boolean newIsDisetujui, UserModel userPenyetuju);
